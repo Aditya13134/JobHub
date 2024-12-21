3a1492ae51283d8a5b851eb8626ee37ab9a6da27
Aaditya Singh <119161277+Aditya13134@users.noreply.github.com>
2024-12-21 16:54:43 +0530

    Delete backend directory

--- backend/schemas/index.js
+++
@@ -1,5 +0,0 @@
-const jobResolver = require("../resolvers/jobResolver");
-const userResolver = require("../resolvers/userResolver");
-const applicationResolver = require("../resolvers/applicationResolver");
-
-module.exports = [jobResolver, userResolver, applicationResolver];
